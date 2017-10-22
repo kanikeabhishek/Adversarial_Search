df804f147aec5b2196ab6cfc52716c79b22229f3
Abhishek Kanike <abkanike@iu.edu>
2017-10-22 18:14:05 -0400

    Prune successors based on dynamic programming.
    1.When successor state is returned with respective alpha or
    beta value, memoize it and reuse when this state is revisited.
    
    2. Format code.

--- part1/pichu.py
+++ part1/pichu.py
@@ -2,21 +2,21 @@
 import sys
 import copy
 
-
-white = ['P','R','B','Q','K','N'];
-black = ['p','r','b','q','k','n'];
-
-# `me` not in use now can be removed or used later.
-me = []
+INF = 99999999999
+MAXDEPTH = 4
+WHITE = ['P','R','B','Q','K','N'];
+BLACK = ['p','r','b','q','k','n'];
+PIECE_WEIGHT = {
+    'P': 1, 'p': -1, 'R': 5, 'r': -5, 'B': 3, 'b': -3, 'N': 3, 'n': -3, 'Q': 9, 'q': -9, 'K': 100, 'k': -100
+}
 
 opposition = []
-
 frontier = []
-maxdepth = int(sys.argv[4])
+visited = {}
 
 def printable_board(board):
-    for i in board:
-        print(i)
+    for row in board:
+        print(row)
     print("--------------------------------------------------------------------------------------")
     pass
 
@@ -41,42 +41,46 @@ def isValidorDisbale(board, row, col, row_list, col_list, i):
         else:
             return False
 
-def generatesuccessor(board,player):
+def generatesuccessor(board, player):
     global frontier
     frontier = []
     identify_opponent(player)
 
     if player:
-        (p,r,b,q,k,n) = white
+        (p, r, b, q, k, n) = WHITE
     else:
-        (p,r,b,q,k,n) = black
+        (p, r, b, q, k, n) = BLACK
 
-    for i in range(0,8):
-        for j in range(0,8):
-            if(board[i][j] == p):
-                move_parakeeth(board,p,i,j)
-            elif(board[i][j] == r):
-                move_robin(board,r,i,j)
-            elif(board[i][j] == b):
-                move_bluejay(board,b,i,j)
-            elif(board[i][j] == q):
-                move_queztal(board,q,i,j)
-            elif(board[i][j] == k):
-                move_kingfisher(board,k,i,j)
-            elif(board[i][j] == n):
-                move_nighthawk(board,n,i,j)
+    for row in range(0, 8):
+        for col in range(0, 8):
+            if(board[row][col] == p):
+                move_parakeeth(board, p, row, col)
+            elif(board[row][col] == r):
+                move_robin(board, r, row, col)
+            elif(board[row][col] == b):
+                move_bluejay(board, b, row, col)
+            elif(board[row][col] == q):
+                move_queztal(board, q, row, col)
+            elif(board[row][col] == k):
+                move_kingfisher(board, k, row, col)
+            elif(board[row][col] == n):
+                move_nighthawk(board, n, row, col)
     return frontier
 
 def add_piece(board, row, col, piece):
     return board[0:row] + [board[row][0:col] + [piece,] + board[row][col+1:]] + board[row+1:]
 
-def move_parakeeth(board,p,row,col):
-    if p in black:
-        q= 'q'
-        Parakeeth_Row = [-1, -1, -1, -2]
+def move_parakeeth(board, p, row, col):
+    if p in BLACK:
+        q = 'q'
+        Parakeeth_Row = [-1, -1, -1]
+        if row == 6:
+            Parakeeth_Row.append(-2)
     else:
-        q= 'Q'
-        Parakeeth_Row = [1, 1, 1, 2]
+        q = 'Q'
+        Parakeeth_Row = [1, 1, 1]
+        if row == 1:
+            Parakeeth_Row.append(2)
 
     Parakeeth_Col = [-1, 0, 1, 0]
 
@@ -101,203 +105,108 @@ def moveBird(board, row, col, possible_row, possible_col, possible_moves, iterat
                 new_board = add_piece(new_board, row1, col1, bird)
                 frontier.append(new_board)
 
-def move_queztal(board,q,row,col):
-    Queztal_Row = [-1, -1, -1, 0, 1, 1, 1, 0]
-    Queztal_Col = [-1, 0, 1, 1, 1, 0, -1, -1]
+def move_queztal(board, q, row, col):
+    Queztal_Row = [1, 1, 1, 0, -1, -1, 0, -1]
+    Queztal_Col = [1, 0,-1, 1, -1, 0, -1, 1]
     moveBird(board, row, col, Queztal_Row, Queztal_Col, len(Queztal_Row), 8, q)
 
-def move_bluejay(board,b,row,col):
+def move_bluejay(board, b, row, col):
     BlueJay_Row = [-1, -1, 1, 1]
     BlueJay_Col = [-1, 1, 1, -1]
     moveBird(board, row, col, BlueJay_Row, BlueJay_Col, len(BlueJay_Row), 8, b)
 
-def move_robin(board,r,row,col):
+def move_robin(board, r, row, col):
     Robin_Row = [0, -1, 0, 1]
     Robin_Col = [-1, 0, 1, 0]
     moveBird(board, row, col, Robin_Row, Robin_Col, len(Robin_Row), 8, r)
 
-def move_kingfisher(board,k,row,col):
+def move_kingfisher(board, k, row, col):
     KingFisher_Row = [1, 1, 1, 0, -1, -1, -1, 0]
     KingFisher_Col = [-1, 0, 1, 1, 1, 0, -1, -1]
     moveBird(board, row, col, KingFisher_Row, KingFisher_Col, len(KingFisher_Row), 2, k)
 
-def move_nighthawk(board,n,row,col):
+def move_nighthawk(board, n, row, col):
     NightHawk_Row = [1, 2, 2, 1, -1, -2, -2, -1]
     NightHawk_Col = [-2, -1, 1, 2, 2 ,1, -1, -2]
     moveBird(board, row, col, NightHawk_Row, NightHawk_Col, len(NightHawk_Row), 2, n)
 
-def iskingcaptured(board,player):
+def iskingcaptured(board, player):
     if(player):
         king = 'K'
     else:
         king = 'k'
-
-    for i in range(0,8):
-        for j in range(0,8):
-            if(board[i][j] == king):
+    for row in range(0, 8):
+        for col in range(0, 8):
+            if(board[row][col] == king):
                 return False
     return True
 
-def aplha_beta_decision(initial_board,player):
-    # generatesuccessor(initial_board,player)#comment and uncomment aplha beta algo
-    maxi = -99999999999
+def alpha_beta_decision(initial_board, player):
+    maxi = -INF
     state = []
     for successor in generatesuccessor(initial_board, player):
-         beta = mini_value(successor, 1, -99999999999, 99999999999, not player)
-         if(maxi <= beta):
+        beta = mini_value(successor, 1, -INF, INF, not player)
+        if(maxi <= beta):
             state = successor
             maxi = beta
     return state
 
-def max_value(state,depth,alpha,beta,player):
-    if(iskingcaptured(state,player)):
-        return -99999999999
+def max_value(state, depth, alpha, beta, player):
+    if(iskingcaptured(state, player)):
+        return -INF
 
-    if(depth == maxdepth ):
-        return piece_square_evaluation(state)
-        #return mobility_evaluation(state,player)
-
+    if(depth == MAXDEPTH ):
+        if(player):
+            return material_evaluation(state)
+        else:
+            return -1 * material_evaluation(state)
     else:
         for successor in generatesuccessor(state, player):
-            alpha = max(alpha,mini_value(successor, depth+1, alpha, beta, not player))
+            if str(successor) in visited:
+                alpha = visited[str(successor)]
+            else:
+                alpha = max(alpha, mini_value(successor, depth + 1, alpha, beta, not player))
+                visited[str(successor)] = alpha
             if(alpha >= beta):
                 return alpha
     return alpha
 
-def mini_value(state,depth,alpha,beta,player):
+def mini_value(state, depth, alpha, beta, player):
 
-    if(iskingcaptured(state,player)):
-        return 99999999999
+    if(iskingcaptured(state, player)):
+        return INF
 
-    if(depth == maxdepth):
-        return piece_square_evaluation(state)
-        #return mobility_evaluation(state,player)
+    if(depth == MAXDEPTH):
+        if(player):
+            return material_evaluation(state)
+        else:
+            return -1 * material_evaluation(state)
     else:
         for successor in generatesuccessor(state, player):
-            beta = min(beta,max_value(successor, depth+1, alpha, beta, not player))
+            if str(successor) in visited:
+                beta = visited[str(successor)]
+            else:
+                beta = min(beta, max_value(successor, depth + 1, alpha, beta, not player))
+                visited[str(successor)] = beta
             if(alpha >= beta):
                 return beta
     return beta
 
-def kingPosistions(board):
-    for i in range(0,8):
-        for j in range(0,8):
-            if (board[i][j] == 'K'):
-                king_white = (i,j)
-            if (board[i][j] == 'k'):
-                king_black = (i,j)
-    return (king_white, king_black)
-
 def material_evaluation(board):
     value = 0
-    for i in range(0,8):
-        for j in range(0,8):
-            if board[i][j] == 'P':
-                value += 1
-            if board[i][j] == 'p':
-                value -= 1
-            if board[i][j] == 'R':
-                value += 5
-            if board[i][j] == 'r':
-                value -= 5
-            if board[i][j] == 'B':
-                value += 3
-            if board[i][j] == 'b':
-                value -= 3
-            if board[i][j] == 'N':
-                value += 3
-            if board[i][j] == 'n':
-                value -= 3
-            if board[i][j] == 'Q':
-                value += 9
-            if board[i][j] == 'q':
-                value -= 9
-            if board[i][j] == 'K':
-                value += 100
-            if board[i][j] == 'k':
-                value -= 100
-    return value
-
-piece_square_table = {
-    'P': [[[1,  1,  1,  1,  1,  1,  1,  1],
-         [50, 50, 50, 50, 50, 50, 50, 50],
-         [10, 10, 20, 30, 30, 20, 10, 10],
-         [5,  5, 10, 25, 25, 10,  5,  5],
-         [1,  1,  1, 15, 20,  1,  1,  1],
-         [5, -5,-10,  1,  1,-10, -5,  5],
-         [5, 10, 10,-20,-20, 10, 10,  5],
-         [1,  1,  1,  1,  1,  1,  1,  1]], 100],
-    'N': [[[-50,-40,-30,-30,-30,-30,-40,-50],
-         [-40,-20,  1,  1,  1,  1,-20,-40],
-         [-30,  1, 10, 15, 15, 10,  1,-30],
-         [-30,  5, 15, 20, 20, 15,  5,-30],
-         [-30,  1, 15, 20, 20, 15,  1,-30],
-         [-30,  5, 10, 15, 15, 10,  5,-30],
-         [-40,-20,  1,  5,  5,  1,-20,-40],
-         [-50,-40,-30,-30,-30,-30,-40,-50]], 320],
-    'B': [[[-20,-10,-10,-10,-10,-10,-10,-20],
-         [-10,  1,  1,  1,  1,  1,  1,-10],
-         [-10,  1,  5, 10, 10,  5,  1,-10],
-         [-10,  5,  5, 10, 10,  5,  5,-10],
-         [-10,  1, 10, 10, 10, 10,  1,-10],
-         [-10, 10, 10, 10, 10, 10, 10,-10],
-         [-10,  5,  1,  1,  1,  1,  5,-10],
-         [-20,-10,-10,-10,-10,-10,-10,-20]], 330],
-    'R': [[[1,  1,  1,  1,  1,  1,  1,  1],
-         [ 5, 10, 10, 10, 10, 10, 10,  5],
-         [-5,  1,  1,  1,  1,  1,  1, -5],
-         [-5,  1,  1,  1,  1,  1,  1, -5],
-         [-5,  1,  1,  1,  1,  1,  1, -5],
-         [-5,  1,  1,  1,  1,  1,  1, -5],
-         [-5,  1,  1,  1,  1,  1,  1, -5],
-         [ 1,  1,  1,  5,  5,  1,  1,  1]], 500],
-    'Q': [[[-20,-10,-10, -5, -5,-10,-10,-20],
-         [-10,  1,  1,  1,  1,  1,  1,-10],
-         [-10,  1,  5,  5,  5,  5,  1,-10],
-         [-5,  1,  5,  5,  5,  5,  1, -5],
-         [-5,  1,  5,  5,  5,  5,  1, -5],
-         [-10,  5,  5,  5,  5,  5,  1,-10],
-         [-10,  1,  5,  1,  1,  5,  1,-10],
-         [-20,-10,-10, -5, -5,-10,-10,-20]], 9000],
-    'K': [[[-30,-40,-40,-50,-50,-40,-40,-30],
-         [-30,-40,-40,-50,-50,-40,-40,-30],
-         [-30,-40,-40,-50,-50,-40,-40,-30],
-         [-30,-40,-40,-50,-50,-40,-40,-30],
-         [-20,-30,-30,-40,-40,-30,-30,-20],
-         [-10,-20,-20,-20,-20,-20,-20,-10],
-         [20, 20,  1,  1,  1,  1, 20, 20],
-         [20, 30, 10,  1,  1, 10, 30, 20]], 20000]
-}
-
-def piece_square_evaluation(board):
-    score = 0
     for row in range(0, 8):
         for col in range(0, 8):
-            current_piece = board[row][col]
-            if current_piece.upper() in piece_square_table.keys():
-                if current_piece.istitle():
-                    row1 = ( row * -1 ) - 1
-                    score += (piece_square_table[current_piece][0][row1][col] * piece_square_table[current_piece][1])
-                else:
-                    current_piece = current_piece.upper()
-                    score -= (piece_square_table[current_piece][0][row][col] * piece_square_table[current_piece][1])
-    return score
-
-def mobility_evaluation(board,player):
-    legal_moves = len(generatesuccessor(board,player))
-    legal_moves_opponent = len(generatesuccessor(board,not player))
-    return legal_moves - legal_moves_opponent
+            if board[row][col] != '.':
+                value += PIECE_WEIGHT[board[row][col]]
+    return value
 
 def identify_opponent(player):
-    global me
     global opposition
 
     if player == True:
-        me = white
-        opposition = black
+        opposition = BLACK
     else:
-        me = black
-        opposition = white
+        opposition = WHITE
 
 def create_board(initial_state):
     board = list();
@@ -306,15 +215,25 @@ def create_board(initial_state):
     return board
 
 def read_input():
+    if (len(sys.argv)<4):
+        print ("Please enter three inputs.\n1.Player turn (w 'or' b)\n2.Board configuration\
+                \n3.Time (in seconds) for algorithm to run.\nExiting...")
+        sys.exit(0)
     turn = sys.argv[1]
     initial_state = sys.argv[2]
     time = int(sys.argv[3])
-    return (turn,initial_state,time)
+    return (turn, initial_state, time)
+
+def validate_input(initial_state, turn):
+    if (len(initial_state) != 64):
+        print ("Please enter valid board... Exiting...")
+        return -1
+    if not turn in ('w', 'b'):
+        print ("Please enter valid player (w 'or' b) turn... Exiting...")
+        return -1
 
 def main():
-    (turn,initial_state,time) = read_input()
-    if (len(initial_state) != 64):
-        return -1
+    (turn, initial_state, time) = read_input()
     inital_board = create_board(initial_state)
     print("\n********************************** INITIAL BOARD **************************************")
     printable_board(inital_board)
@@ -324,15 +243,15 @@ def main():
     else:
         player = False
 
-    next_move = aplha_beta_decision(inital_board,player)
+    next_move = alpha_beta_decision(inital_board, player)
 
     print("\n*********************************** FINAL BOARD **************************************")
     printable_board(next_move)
     print("**************************************************************************************\n")
     next_move_str = ''
-    for i in range(0,8):
-        for j in range(0,8):
-            next_move_str += next_move[i][j]
+    for row in range(0, 8):
+        for col in range(0, 8):
+            next_move_str += next_move[row][col]
     print(next_move_str)
 
 if __name__=="__main__":
